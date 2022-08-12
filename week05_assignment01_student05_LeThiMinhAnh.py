60a929e9641a96140015b8707c00b923a4c33e2c
= <lethiminhanh2_t66@hus.edu.vn>
2022-08-12 16:25:19 +0700

    add week05 assignment01

---
+++ week05_assignment01_student05_LeThiMinhAnh.py
@@ -0,0 +1,12 @@
+'''
+Repository: https://github.com/sunahiri25/week05-assignment
+
+Danh sách công việc đã thực hiện:
+- Thực hiện commit 
+- Tạo nhánh, tạo tag trên repo
+- Tạo issue, đóng issue
+- Tạo pull request
+- Chọn license cho repo
+- Viết README
+
+'''
